99ba2e1b117eb3366d61050d06cb72dfb3e61c2d
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-07-04 22:49:50 +0530

    hooks in jest

--- __test__/checkForTruthy.test.js
+++ __test__/checkForTruthy.test.js
@@ -1,8 +1,11 @@
 test('check truthy', () => { 
     var name="kirtikumar"
     var number=null
+    let randomNumber=10
     expect(number).toBeNull()
     expect(name).not.toBeNull()
     expect(name).toBeTruthy()
     expect(number).toBeFalsy()
+
+    expect(randomNumber).toBeLessThan(20)
  })
