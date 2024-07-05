400d20ebc0459c431b28b6990ac8f113ad3bdf41
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-07-05 16:19:45 +0530

    added coverage criteria

--- __test__/hooks.test.js
+++ __test__/hooks.test.js
@@ -1,31 +1,27 @@
-const mathOperation=require("../calculator")
-describe('hooks calculator', () => {
-beforeEach(()=>{
-console.log("before each called") 
-})
+const mathOperation = require("../calculator");
+describe("hooks calculator", () => {
+  beforeEach(() => {
+    console.log("before each called");
+  });
 
-afterEach(()=>{
-console.log("after each called") 
-    
-})
+  afterEach(() => {
+    console.log("after each called");
+  });
 
-beforeAll(()=>{
-    console.log("before all called") 
+  beforeAll(() => {
+    console.log("before all called");
+  });
 
-})
+  afterAll(() => {
+    console.log("after all called");
+  });
 
-afterAll(()=>{
-console.log("after all called") 
+  test("adding 4 and 6", () => {
+    expect(mathOperation.add(4, 6)).toBe(10);
+    expect(mathOperation.add(7, 4)).toBe(11);
+  });
 
-})
-
-test('adding 4 and 6', () => { 
-    expect(mathOperation.add(4,6)).toBe(10)
-    expect(mathOperation.add(7,4)).toBe(11)
- })
-
- test('adding 4 and 6', () => { 
-    expect(mathOperation.sub(4,6)).toBe(-2)
- })
-    
+  test("adding 4 and 6", () => {
+    expect(mathOperation.sub(4, 6)).toBe(-2);
+  });
 });
